8e10e5c178aba5ed704b0c9ece708ba48be3df94
Gustavo Cruz <gustavoindeolcruz@gmail.com>
2025-01-03 22:52:17 -0300

    finish `/api/v1/status` endpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,32 @@
 import database from "infra/database.js";
 
 async function status(req, res) {
-  const result = await database.query("SELECT 1 + 1 as sum;");
-  console.log(result.rows);
-  res.status(200).send();
+  const updated_at = new Date().toISOString();
+
+  let qryRes = await database.query("SHOW server_version;");
+  const version = qryRes.rows[0].server_version;
+
+  qryRes = await database.query("SHOW max_connections;");
+  const max_connections = parseInt(qryRes.rows[0].max_connections);
+
+  qryRes = await database.query({
+    text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;",
+    values: [process.env.POSTGRES_DB],
+  });
+  const opened_connections = qryRes.rows[0].count;
+
+  const status = {
+    updated_at,
+    dependencies: {
+      database: {
+        version,
+        max_connections,
+        opened_connections,
+      },
+    },
+  };
+
+  res.status(200).json(status);
 }
 
 export default status;
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,4 +1,24 @@
 test("GET to /api/v1/status should return 200", async () => {
   const res = await fetch("http:localhost:3000/api/v1/status");
   expect(res.status).toBe(200);
+
+  const body = await res.json();
+
+  const updatedAt = body["updated_at"];
+  expect(updatedAt).toBeDefined();
+  const parsedUpdatedAt = new Date(updatedAt).toISOString();
+  expect(parsedUpdatedAt).toEqual(updatedAt);
+
+  const version = body.dependencies.database["version"];
+  expect(version).toBeDefined();
+  expect(typeof version).toBe("string");
+
+  const maxConnections = body.dependencies.database["max_connections"];
+  expect(maxConnections).toBeDefined();
+  expect(typeof maxConnections).toBe("number");
+
+  const openedConnections = body.dependencies.database["opened_connections"];
+  expect(openedConnections).toBeDefined();
+  expect(typeof openedConnections).toBe("number");
+  expect(openedConnections).toEqual(1);
 });
