8304a3b36badb0cb2f97838361230a35a064beca
Gustavo Cruz <gustavoindeolcruz@gmail.com>
2025-01-08 20:30:19 -0300

    fix `/api/v1/migrations` endpoint db connection leaking

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -3,36 +3,46 @@ import { join } from "node:path";
 import database from "infra/database";
 
 export default async function migrations(req, res) {
-  const dbClient = await database.getNewClient();
-  const defaultMigrationOptions = {
-    dbClient,
-    dir: join("infra", "migrations"),
-    direction: "up",
-    dryRun: true,
-    verbose: true,
-    migrationsTable: "pgmigrations",
-  };
-
-  if (req.method === "GET") {
-    const pendingMigrations = await migrationRunner({
-      ...defaultMigrationOptions,
-    });
-    await dbClient.end();
-
-    return res.status(200).json(pendingMigrations);
+  const allowedMethods = ["GET", "POST"];
+  if (!allowedMethods.includes(req.method)) {
+    return res.status(405).end();
   }
 
-  if (req.method === "POST") {
-    const migratedMigrations = await migrationRunner({
-      ...defaultMigrationOptions,
-      dryRun: false,
-    });
+  let dbClient;
+  try {
+    dbClient = await database.getNewClient();
+
+    const defaultMigrationOptions = {
+      dbClient,
+      dir: join("infra", "migrations"),
+      direction: "up",
+      dryRun: true,
+      verbose: true,
+      migrationsTable: "pgmigrations",
+    };
+
+    if (req.method === "GET") {
+      const pendingMigrations = await migrationRunner({
+        ...defaultMigrationOptions,
+      });
+
+      return res.status(200).json(pendingMigrations);
+    }
+
+    if (req.method === "POST") {
+      const migratedMigrations = await migrationRunner({
+        ...defaultMigrationOptions,
+        dryRun: false,
+      });
+
+      return migratedMigrations.length === 0
+        ? res.status(200).json(migratedMigrations)
+        : res.status(201).json(migratedMigrations);
+    }
+  } catch (err) {
+    console.error(err);
+    throw err;
+  } finally {
     await dbClient.end();
-
-    return migratedMigrations.length === 0
-      ? res.status(200).json(migratedMigrations)
-      : res.status(201).json(migratedMigrations);
   }
-
-  return res.status(405).end();
 }
