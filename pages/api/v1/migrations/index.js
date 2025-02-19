d711bb33c294fc58c854242b25f9312ed911fc19
Gustavo Cruz <gustavoindeolcruz@gmail.com>
2025-02-19 19:00:33 -0300

    refactor: make `/api/v1/migrations` use `migrator` model

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -1,57 +1,20 @@
 import { createRouter } from "next-connect";
-import migrationRunner from "node-pg-migrate";
-import { resolve } from "node:path";
-import database from "infra/database";
 import { errorHandlers } from "infra/controller";
+import migrator from "models/migrator";
 
 const router = createRouter();
 
 router.get(async (req, res) => {
-  let dbClient;
-  try {
-    dbClient = await database.getNewClient();
-    const defaultMigrationOptions = {
-      dbClient,
-      dir: resolve("infra", "migrations"),
-      direction: "up",
-      dryRun: true,
-      verbose: true,
-      migrationsTable: "pgmigrations",
-    };
-
-    const pendingMigrations = await migrationRunner({
-      ...defaultMigrationOptions,
-    });
-
-    return res.status(200).json(pendingMigrations);
-  } finally {
-    await dbClient?.end();
-  }
+  const pendingMigrations = await migrator.listPendingMigrations();
+  return res.status(200).json(pendingMigrations);
 });
 
 router.post(async (req, res) => {
-  let dbClient;
-  try {
-    dbClient = await database.getNewClient();
-    const defaultMigrationOptions = {
-      dbClient,
-      dir: resolve("infra", "migrations"),
-      direction: "up",
-      dryRun: false,
-      verbose: true,
-      migrationsTable: "pgmigrations",
-    };
+  const migratedMigrations = await migrator.runPendingMigrations();
 
-    const migratedMigrations = await migrationRunner({
-      ...defaultMigrationOptions,
-    });
-
-    return migratedMigrations.length === 0
-      ? res.status(200).json(migratedMigrations)
-      : res.status(201).json(migratedMigrations);
-  } finally {
-    await dbClient?.end();
-  }
+  return migratedMigrations.length === 0
+    ? res.status(200).json(migratedMigrations)
+    : res.status(201).json(migratedMigrations);
 });
 
 export default router.handler(errorHandlers);
